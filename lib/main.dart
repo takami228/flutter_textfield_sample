4e8c38f122c7ab2c07ae191403eee1eece057cab
takami228 <m.takami228@gmail.com>
2019-12-14 19:54:50 +0900

    Updtae form

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,90 @@ class _AutoCorrectTestState extends State<AutoCorrectTest> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-            'TextField with autocorrect: false and enableSuggestions: false'),
+        appBar: AppBar(
+          title: Text('ID Pass Form Sample'),
+        ),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(padding: EdgeInsets.all(20.0)),
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    maxLength: 16,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      filled: true,
+                      helperText: 'No more than 16 characters.',
+                      labelText: 'UserName *',
+                    ),
+                  )),
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: PasswordField(
+                    helperText: 'No more than 8 characters.',
+                    labelText: 'Password *',
+                  )),
+            ]));
+  }
+}
+
+class PasswordField extends StatefulWidget {
+  const PasswordField({
+    this.fieldKey,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.onSaved,
+    this.validator,
+    this.onFieldSubmitted,
+  });
+
+  final Key fieldKey;
+  final String hintText;
+  final String labelText;
+  final String helperText;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+  final ValueChanged<String> onFieldSubmitted;
+
+  @override
+  _PasswordFieldState createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      key: widget.fieldKey,
+      obscureText: _obscureText,
+      autocorrect: false,
+      maxLength: 8,
+      keyboardType: TextInputType.visiblePassword,
+      onSaved: widget.onSaved,
+      validator: widget.validator,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      decoration: InputDecoration(
+        border: const UnderlineInputBorder(),
+        filled: true,
+        hintText: widget.hintText,
+        labelText: widget.labelText,
+        helperText: widget.helperText,
+        suffixIcon: GestureDetector(
+          onTap: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+          child: Icon(
+            _obscureText ? Icons.visibility : Icons.visibility_off,
+            semanticLabel: _obscureText ? 'show password' : 'hide password',
+          ),
+        ),
       ),
-      body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: TextField(
-              obscureText: true,
-              autocorrect: false,
-              enableSuggestions: false,
-              keyboardType: TextInputType.visiblePassword)),
     );
   }
 }
